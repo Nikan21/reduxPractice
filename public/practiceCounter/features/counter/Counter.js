3baab0be59b33318e8bf0281878adfe875f58bdb
Dmitriy <dimonnikanorov2@gmail.com>
2022-11-09 13:51:55 +0200

    Practice redux toolkit and react redux on example

---
+++ public/practiceCounter/features/counter/Counter.js
@@ -0,0 +1,21 @@
+import {useSelector, useDispatch} from 'react-redux'
+import {increment, decrement} from './counterSlice'
+
+export function Counter() {
+    const count = useSelector(state => state.counter.value)
+    const dispatch = useDispatch()
+
+    return(
+        <div>
+            <div>
+                <button onClick={() => dispatch(increment())}> 
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button onClick={() => dispatch(decrement())}>
+                    Decrement
+                </button>
+            </div>
+        </div>
+    )
+}
